dcfd8e0454d91920443c5bfca56e743c4a5b95df
向阳 <91933474+XiangYyang@users.noreply.github.com>
2026-04-27 22:10:50 +0800

    fix: load sheets in subdirectory from Codeberg VFS (#189)

--- src/kicanvas/services/codeberg-vfs.ts
+++ src/kicanvas/services/codeberg-vfs.ts
@@ -4,30 +4,52 @@
     Full text available at: https://opensource.org/licenses/MIT
 */
 import { base64_decode } from "../../base/base64";
-import { initiate_download } from "../../base/dom/download";
-import { extension } from "../../base/paths";
-import { Codeberg, GetBlobResponse, RepoContentResponse } from "./codeberg";
-import { type IFileSystem } from "./vfs";
+import { based_on, normalize_join } from "../../base/paths";
+import {
+    Codeberg,
+    CodebergRepoInfo,
+    GetBlobResponse,
+    RepoContentResponse,
+} from "./codeberg";
+import { FileEntry, FileSystemBase } from "./vfs";
 
-export class CodebergFileSystem implements IFileSystem {
-    static readonly kicad_extensions = ["kicad_pcb", "kicad_pro", "kicad_sch"];
+export class CodebergFileSystem extends FileSystemBase {
+    private download_urls: Map<string, URL> = new Map();
 
-    async setup() {}
-    constructor(private files_to_urls: Map<string, URL>) {}
+    constructor(private repo: CodebergRepoInfo) {
+        super();
+    }
 
-    public static async fromURLs(
-        url: string | URL,
-    ): Promise<CodebergFileSystem | null> {
-        const files_to_urls = new Map<string, URL>();
-
-        const info = Codeberg.parse_url(url);
-        if (!info) {
-            return null;
+    async load_file(path: string): Promise<File> {
+        const url = this.download_urls.get(path);
+        if (!url) {
+            throw new Error(`File ${path} not found.`);
         }
 
+        // API: https://codeberg.org/api/swagger#/repository/GetBlob
+        const blob = await Codeberg.request_json<GetBlobResponse>(url.pathname);
+
+        if (blob.content.length === 0) {
+            throw new Error(`Blob returns empty when loading file ${path}`);
+        }
+
+        if (blob.encoding !== "base64") {
+            throw new Error(`Unsupported encoding: ${blob.encoding}`);
+        }
+
+        const content = base64_decode(blob.content);
+        const file = new File([content], path);
+
+        return file;
+    }
+
+    async enumerate(cur_path: string): Promise<FileEntry[]> {
         // API:
         // https://codeberg.org/api/swagger#/repository/repoGetContents
-        const api_url = `repos/${info.owner}/${info.repo}/contents/${info.path}`;
+        const info = this.repo;
+        const base_dir = info.path;
+        const base_api = `repos/${info.owner}/${info.repo}/contents`;
+        const api_url = normalize_join(base_api, base_dir, cur_path);
 
         let files = await Codeberg.request_json<
             RepoContentResponse | RepoContentResponse[]
@@ -37,58 +59,39 @@ export class CodebergFileSystem implements IFileSystem {
             files = [files];
         }
 
-        for (const file of files) {
-            if (!file.name || !file.git_url || file.type !== "file") {
-                continue;
-            }
-
+        const result: FileEntry[] = [];
+        for (const it of files) {
             if (
-                !CodebergFileSystem.kicad_extensions.includes(
-                    extension(file.name),
-                )
+                it.type === "file" &&
+                CodebergFileSystem.is_kicad_file(it.name)
             ) {
-                continue;
-            }
+                const file_path = based_on(base_dir, it.path);
 
-            files_to_urls.set(file.name, new URL(file.git_url));
+                this.download_urls.set(file_path, new URL(it.git_url));
+
+                result.push({
+                    type: "file",
+                    path: file_path,
+                });
+            } else if (it.type === "dir") {
+                result.push({
+                    type: "directory",
+                    path: based_on(base_dir, it.path),
+                });
+            }
         }
 
-        if (files_to_urls.size == 0) {
-            // no valid URL and files, return null.
+        return result;
+    }
+
+    public static async fromURLs(
+        url: string | URL,
+    ): Promise<CodebergFileSystem | null> {
+        const info = Codeberg.parse_url(url);
+        if (!info) {
             return null;
         }
 
-        return new CodebergFileSystem(files_to_urls);
-    }
-
-    *list(): Generator<string> {
-        yield* this.files_to_urls.keys();
-    }
-
-    async get(name: string) {
-        const url = this.files_to_urls.get(name);
-        if (!url) {
-            throw new Error(`File ${name} not found.`);
-        }
-
-        // API: https://codeberg.org/api/swagger#/repository/GetBlob
-        const blob = await Codeberg.request_json<GetBlobResponse>(url.pathname);
-
-        if (blob.encoding !== "base64") {
-            throw new Error(`Unsupported encoding: ${blob.encoding}`);
-        }
-
-        const content = base64_decode(blob.content);
-        const file = new File([content], name);
-
-        return file;
-    }
-
-    async has(name: string) {
-        return Promise.resolve(this.files_to_urls.has(name));
-    }
-
-    async download(name: string) {
-        initiate_download(await this.get(name));
+        return new CodebergFileSystem(info);
     }
 }
